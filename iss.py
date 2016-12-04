207ddb2028306857b1b120649aab83bd64ce77cb
Vasyl <stetsyuk.vasyl@gmail.com>
2016-12-04 00:23:51 -0600

    Changed Virtualenv python version from 2.7 -> 3.5.2

--- iss.py
+++ iss.py
@@ -1,7 +1,20 @@
 # ISS API
-
+"""
 import requests
 
+req = requests.get("http://api.open-notify.org/iss-now.json?")
 
-response = requests.get("http://api.open-notify.org/iss-now.json")
-print(response.status_code) 
+#print(req.status_code)
+print(req.content, req.status_code)
+"""
+
+import urllib2
+import json
+
+req = urllib2.Request("http://api.open-notify.org/iss-now.json")
+response = urllib2.urlopen(req)
+
+obj = json.loads(response.read())
+
+print obj['timestamp']
+print obj['iss_position']['latitude'], obj['data']['iss_position']['latitude']
