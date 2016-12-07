97c205093695d024ca3ba5b1823e34e6f4513ca6
Vasyl <stetsyuk.vasyl@gmail.com>
2016-12-07 16:46:24 -0600

    http status codes now have text explenation

--- iss.py
+++ iss.py
@@ -1,15 +1,23 @@
-
-# ISS API
-# 
 import requests
 
-r = requests.get("http://api.open-notify.org/iss-now.json?")
+r = requests.get("https://api.open-notify.org/iss-now.json")
+status = r.status_code
 
-q = input("ISS API, what is you question about?: ")
-if q == "connection":
-    print("ISS API conection status is:", r.status_code)
-
+# Replasing http status code with text
+if status == 200:
+    ConStatus = "200 - Connection is OK!"
+elif status == 400:
+    ConStatus = "400 - Bad Request.\n(Please check your request code for correctness.)"
+elif status == 404:
+    ConStatus == "404 - Not found.\n(The server has not found anything matching the Request-URI.)"
+elif status == 500:
+    ConStatus == "500 - Internal Server Error.\n(The server encountered an unexpected condition which prevented it from fulfilling the request.)"
 else:
-	print("Connection status is: ", r.status_code, "\n" "All ISS API data: \n", r.text)
+    ConStatus = "Uncnown HTTP status code.\n(http status code doesn't match our records.)"
 
-#print(r.status_code, "\n", r.text)
+# UI
+q = input('ISS API \n Type "cnnection" to see conncetion status:\n')
+if q == "connection":
+    print("ISS API conection status is:\n", ConStatus)
+else:
+	print("Ooops, you entered incorrect command \nor \nwe can't check ISS conection status at the moment")
